304d95f6380dc2a91b5c78b3a0d108991aff37d9
leewoojin <zmfpdl64@naver.com>
2023-02-12 16:05:43 +0900

    인턴쉽 dfs문제 해결완료

--- 프로그래머스/test/test6.py
+++ 프로그래머스/test/test6.py
@@ -1,19 +1,42 @@
 def solution(grid):
+    global map1
     for i in range(len(grid)):
-        grid[i] = list(grid[i])
-        print(grid[i])
-    for i in range(len(grid)):
-        for j in range(grid[i]):
-            if grid[i][j] == '.':
-                for k in move:
-                    x = k[0]
-                    y = k[1]
+        map1.append(list(grid[i]))
+    for i in range(len(map1[0])):
+        if map1[0][i] == '.':
+            dfs(0, i)
+        if map1[len(map1)-1][i] == '.':
+            dfs(len(map1)-1, i)
+    for i in range(len(map1)):
+        if map1[i][0] == '.':
+            dfs(i, 0)
+        if map1[i][len(map1)-1] == '.':
+            dfs(i, 0)
+    count = 0
+    for i in range(len(map1)):
+        for j in range(len(map1[i])):
+            if map1[i][j] != '1':
+                count += 1
+    print(map1)
+    print(count)
+
+def dfs(x, y):
+    global map1
+    try:
+        if map1[x][y] != '.':
+            return
+        else:
+            map1[x][y] = '1'
+    except:
+        return
+
+    for i in move:
+        a, b  = i[0],i[1]
+        dfs(x+a, y+b)
 
 
-
-
-
-solution([".#.","#..",".#."])
-# solution([".....####", "..#...###", ".#.##..##", "..#..#...", "..#...#.."
-#          , "...###..."])
 move = [[1,0],[0,1],[-1,0],[0,-1]]
+map1 = []
+# solution([".#.","#..",".#."])
+solution([".....####", "..#...###", ".#.##..##", "..#..#...", "..#...#.."
+         , "...###..."])
