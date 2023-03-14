b8c609675dc434127e1597493b0d07b17b594a0a
woojin <zmfpdl64@naver.com>
2023-03-14 22:49:55 +0900

    백준 기초2 그래프1

--- 백준/기초2/그래프/7576토마토.py
+++ 백준/기초2/그래프/7576토마토.py
@@ -7,12 +7,13 @@ t_map = [list(map(int, input().split())) for _ in range(h)]
 vi_map = copy.deepcopy(t_map)
 move = [[0,1],[0,-1],[1,0],[-1,0]]
 pos = []
+one = 0
 q = d([])
 for i in range(len(t_map)):
     for j in range(len(t_map[i])):
         if t_map[i][j] == 1:
+            one += 1
             q.append([i,j,0])
-# 1건들면 안되고 둘중에 최솟값
 while q:
     x, y, t = q.popleft()
     for xx, yy in move:
@@ -27,12 +28,17 @@ while q:
                 if t_map[nx][ny] > (t+1):
                     t_map[nx][ny] = t+1
                     q.append([nx,ny,t+1])
-mini = 0
+mini = -1
+count = 0
 for i in t_map:
     for j in i:
         if j == 0:
             print(-1)
             exit(0)
+        elif j == -1:
+            count +=1
     mini = max(mini, max(i))
-print(t_map)
-print(mini)
+if w*h - count == one:
+    print(0)
+else:
+    print(mini)
