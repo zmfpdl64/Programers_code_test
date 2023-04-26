07aae45814ad2f1e0ed30544373a0d8422b2ee40
woojin <zmfpdl64@naver.com>
2023-04-26 19:59:19 +0900

    프로그래머스 레벨2 택배배달과수거하기

---
+++ 프로그래머스/2단계/택배배달과수거하기.py
@@ -0,0 +1,17 @@
+# 20시 20분
+
+def solution(cap, n, deli, pickups):
+    deli = deli[::-1]
+    pickups = pickups[::-1]
+    result = 0
+    p = 0
+    d = 0
+    for i in range(n):
+        p += pickups[i]
+        d += deli[i]
+        while p > 0 or d > 0:
+            p -= cap
+            d -= cap
+            result += (n-i)*2
+    return result
+print(solution(4, 5, [1,0,3,1,2], [0,3,0,4,0]))
