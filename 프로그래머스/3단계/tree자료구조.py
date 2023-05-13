f4f2d4e1e812bcac2cacf121bca1da2455e9c700
leewoojin <zmfpdl64@naver.com>
2023-05-13 18:09:38 +0900

    프로그래머스 lv3 트리자료구조 해결

--- 프로그래머스/3단계/tree자료구조.py
+++ 프로그래머스/3단계/tree자료구조.py
@@ -1,11 +1,9 @@
-# 15시 7분
-# https://school.programmers.co.kr/learn/courses/30/lessons/42892
+
 
 class Node:
-    def __init__(self, x, y, value=None, left=None, right=None, parent=None):
+    def __init__(self, x, y, left=None, right=None, parent=None):
         self.x = x
         self.y = y
-        self.value = value
         self.left = left
         self.right = right
         self.parent = parent
@@ -29,12 +27,12 @@ class Node:
                     # print('right')
                 else:
                     self.right.insert(node)
-        else:  # 현재 들어온 노드가 해당 노드보다 클 때
+        else: # 현재 들어온 노드가 해당 노드보다 클 때
             # print('bigger')
             if node.x > self.x:
-                node.left = self  # 현재 노드 입력 노드의 자식 노드로 변경
-                node.parent = self.parent  # 현재 노드의 부모 노드를 입력 노드의 부모로 저장
-                self.parent = node  # 현재 노드의 부모 노드를 입력 노드로 변경
+                node.left = self # 현재 노드 입력 노드의 자식 노드로 변경
+                node.parent = self.parent # 현재 노드의 부모 노드를 입력 노드의 부모로 저장
+                self.parent = node # 현재 노드의 부모 노드를 입력 노드로 변경
                 # print('self left')
                 if self.right is not None and self.right.x > node.x:
                     node.right = self.right
@@ -52,45 +50,40 @@ class Node:
                     self.left.parent = node
                     self.left = None
         return
-
-    def preorder(self, node, res):
-        # print(node.x, node.y)
-        res.append(node.value)
+    def preorder(self, node):
+        print(node.x, node.y)
         if node.left is not None:
-            node.preorder(node.left, res)
+            node.preorder(node.left)
         if node.right is not None:
-            node.preorder(node.right, res)
-
-    def inorder(self, node, res):
+            node.preorder(node.right)
+    def inorder(self, node):
         if node.left is not None:
-            node.inorder(node.left, res)
-        # print(node.x, node.y)
-        res.append(node.value)
+            node.inorder(node.left)
+        print(node.x, node.y)
         if node.right is not None:
-            node.inorder(node.right, res)
-
-    def postorder(self, node, res):
+            node.inorder(node.right)
+    def postorder(self, node):
         if node.left is not None:
-            node.postorder(node.left, res)
+            node.postorder(node.left)
         if node.right is not None:
-            node.postorder(node.right, res)
-        # print(node.x, node.y)
-        res.append(node.value)
+            node.postorder(node.right)
+        print(node.x, node.y)
+
+def solution(nodes):
+    nodes.sort(key=lambda x: (-x[1], x[0]))
+    node1 = Node(8, 6)
+    for i in range(0, len(nodes)):
+        x, y = nodes[i]
+        n = Node(x, y)
+        node1.insert(n)
+    # node2 = Node(8, 6)
+    # node1.insert(node2)
+    node1.preorder(node1)
+    print()
+    node1.inorder(node1)
+    print()
+    node1.postorder(node1)
 
 
-def solution(nodeinfo):
-    answer = [[]]
-    pre = []
-    post = []
-    for i, value in enumerate(nodeinfo):
-        nodeinfo[i] = [value[0],value[1],i+1]
-    nodeinfo.sort(key=lambda x: (-x[1], x[0]))
-    node1 = Node(nodeinfo[0][0], nodeinfo[0][1], nodeinfo[0][2])
-    for i in range(1, len(nodeinfo)):
-        x, y, v = nodeinfo[i]
-        node1.insert(Node(x, y, v))
-    node1.preorder(node1, pre)
-    node1.postorder(node1, post)
-    return [pre, post]
-
-print(solution([[5,3],[11,5],[13,3],[3,5],[6,1],[1,3],[8,6],[7,2],[2,2]]))
+solution([[5, 3], [11, 5], [13, 3], [3, 5], [6, 1], [1, 3],  [7, 2], [2, 2]])
+#[8, 6],
