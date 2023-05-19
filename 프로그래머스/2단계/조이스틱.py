7f760d7bc8c5085ec6efaa159a47a0f023f12d07
leewoojin <zmfpdl64@naver.com>
2023-05-19 14:45:54 +0900

    프로그래머스 lv2 조이스틱 해결

--- 프로그래머스/2단계/조이스틱.py
+++ 프로그래머스/2단계/조이스틱.py
@@ -1,36 +1,14 @@
-# 미해결
-from itertools import permutations as permu
-
-alpha = {chr(i): i - 65 for i in range(65, 91)}
-
-
-def minlen(pos, target):
-    mini = min(pos, alpha[target])
-    maxi = max(pos, alpha[target])
-    print(abs(maxi - mini), mini + (25 - maxi))
-    return min(abs(maxi - mini), mini + (25 - maxi))
-
+# 복습 필수
+# 14시 45분 시간 미정...
 
 def solution(name):
-    answer = float('inf')
-    for case in permu(range(len(name)), len(name)):
-        print(case)
-        movelen = 0
-        pos = 0
-        for i in range(len(case) - 1):
-            mini = min(case[i], case[i + 1])
-            maxi = max(case[i], case[i + 1])
-            movelen += min(maxi - mini, abs(mini + len(name) - 1 - maxi))
-            print(min(maxi - mini, abs(mini + len(name) - 1 - maxi)))
-            movelen += minlen(pos, name[i])
-            print()
-
-            # print(movelen)
-            pos = alpha[name[i]]
-        movelen += minlen(pos, name[case[-1]])
-        if answer >= movelen:
-            print(movelen)
-        answer = min(answer, movelen)
-    return answer
-
-
+    idx, answer = 0, 0
+    change = [min(ord(i) - ord('A'), ord('Z')-ord(i)+1) for i in name]
+    movelen = float('inf')
+    answer = sum(change)
+    for i in range(len(name)):
+        count = i+1
+        while count < len(name) and name[count] == 'A':
+            count += 1
+        movelen = min([ movelen, 2 * i + len(name) - count, 2*(len(name)-count)+i])
+    return answer + movelen
