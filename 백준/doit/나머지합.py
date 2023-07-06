993a365f2e939395785cebbd0e4bc45ae63ca95d
woojin <zmfpdl64@naver.com>
2023-07-06 15:37:54 +0900

    나머지합 dict이용해서 풀기

--- 백준/doit/나머지합.py
+++ 백준/doit/나머지합.py
@@ -1,4 +1,3 @@
-# 19시 57부
 # 나머지합
 # https://www.acmicpc.net/problem/10986
 import sys
@@ -7,20 +6,40 @@ n, m = map(int, input().split())
 a = list(map(int, input().split()))
 dic = {}
 dp = [0] * n
-dp[0] = a[0]%m
-dp2 = [0] * 1_000_001
+dp[0] = a[0] % m
+dic[dp[0]] = 1
 answer = 0
 for i in range(1, len(a)):
-    dp[i] = (dp[i-1] + a[i]) % m
+    dp[i] = (dp[i - 1] + a[i]) % m
+    num = dp[i]
     if dp[i] not in dic:
         dic[dp[i]] = 1
     else:
         dic[dp[i]] += 1
-
-for i in range(1, 1_000_001):
-    dp2[i] = dp2[i-1] + i
 for i in dic.values():
-    answer += dp2[i]
+    answer += i * (i - 1) // 2
+if 0 in dic:
+    answer += dic[0]
+
 print(answer)
 
 
+# def solution(n, m, a):
+#     dic = {}
+#     dp = [0] * n
+#     dp[0] = a[0]%m
+#     dic[dp[0]] = 1
+#     answer = 0
+#     for i in range(1, len(a)):
+#         dp[i] = (dp[i-1] + a[i]) % m
+#         num = dp[i]
+#         print(num)
+#         if dp[i] not in dic:
+#             dic[dp[i]] = 1
+#         else:
+#             dic[dp[i]] += 1
+#     for i in dic.values():
+#         answer += i*(i-1)//2
+#     print(answer)
+#
+# solution(6, 3, [3,3,3,1,1,1])
